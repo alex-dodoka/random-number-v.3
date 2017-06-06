b0da1a371fd2db47d6dd766ca117944b50925b3c
alex-dodoka <neksus.92@mail.ru>
2017-06-06 21:56:14 +0300

    clear code: delete "+" before the variable value.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function getRandomInteger() {
-    let min = +$('#min').val(),
-        max = +$('#max').val(),
+    let min = parseInt($('#min').val()),
+        max = parseInt($('#max').val()),
         res = Math.floor(min + Math.random() * (max + 1 - min));
     console.log(res);
 }
